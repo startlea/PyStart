c1f5743af291373e676a84a9a457f38f38e8445c
startlea <eos1984kowalska@gmail.com>
2023-10-28 22:07:48 +0200

    new files 16

--- Moduł-2/16_lancuch_znakow.py
+++ Moduł-2/16_lancuch_znakow.py
@@ -1,6 +1,7 @@
 print('Program podaje numer ASCII przyporządkowany do daneego znaku')
 characters = input('Podaj jakieś słowo: ')
 
+
 char_list = list(characters)
 for char in char_list:
     print(char, ord(char))
