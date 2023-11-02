520366a1538d32806d7428fe4c0a58cecc714f57
startlea <eos1984kowalska@gmail.com>
2023-11-02 14:09:15 +0100

    new files 18

---
+++ Moduł-2/18_tupla.py
@@ -0,0 +1,11 @@
+list = tuple(range(12, 1025))
+#average = sum(grades) / len(grades)
+
+for tuple in list:
+ print(tuple)
+print('Liczb w tupli jest ', (len(range(12, 1025, 6))))
+print('Pierwsze trzy liczby to: ', list[:3])
+print('Przedostatnia liczba to: ', list[-2::2])
+print('Co szósty element licząc od czwartego to: ', list[3::6])
+print('Liczb w tupli jest ', list[::-3])
+print('Średnia ostatnich 10 wartości wynosi: ', sum(list[-10::]) / len(list[-10::]) )
